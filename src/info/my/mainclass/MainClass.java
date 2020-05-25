b270d034eae603d60a4edf4862cd6778de9ca4ae
Anatoly <a_gavrik@bk.ru>
2020-05-26 01:25:00 +0300

    Add collection ArrayList with shapes; and loggers with min and max areas

--- src/info/my/mainclass/MainClass.java
+++ src/info/my/mainclass/MainClass.java
@@ -1,22 +1,57 @@
 package info.my.mainclass;
 
+import info.my.AbstractShape;
+import info.my.Shape;
 import info.my.circle.Circle;
 import info.my.square.Square;
 import info.my.triangle.Triangle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class MainClass {
-   static Logger log=Logger.getLogger(MainClass.class.getName());
-    public static void main(String[] args) {
-        Circle c1= new Circle(15.5);
-        Square sq1= new Square(20);
-        Triangle tr1= new Triangle(14.5, 20.0, 30.7);
+    static Logger log = Logger.getLogger(MainClass.class.getName());
 
-        log.info("Area of circle - "+ c1.getArea());
-        log.info("Area of triangle - "+ tr1.getArea());
-        log.info("Area of square - "+ sq1.getArea());
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(15.5);
+        Square square1 = new Square(20);
+        Triangle triangle1 = new Triangle(14.5, 20.0, 30.7);
+        Circle circle2 = new Circle(30);
+        Square square2 = new Square(5);
+        Triangle triangle2 = new Triangle(30, 20.0, 30);
+
+        log.info("Area of circle - " + circle1.getArea());
+        log.info("Area of triangle - " + triangle1.getArea());
+        log.info("Area of square - " + square1.getArea());
+
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(circle1);
+        shapeList.add(square1);
+        shapeList.add(triangle1);
+        shapeList.add(circle2);
+        shapeList.add(square2);
+        shapeList.add(triangle2);
+
+
+        Shape withMinArea=null;
+        Shape withMaxArea=null;
+
+        for (int i = 0; i < shapeList.size() ; i++) {
+            for (int n = i+1; n < shapeList.size() ; n++){
+               Shape first= shapeList.get(i);
+               Shape second= shapeList.get(n);
+                    if(first.getArea()>second.getArea()){
+                        withMaxArea=first;
+                    } else {withMinArea=first;}
+                }
+            }
+        log.info("Type of shape with max area  - " + withMaxArea.getClass().getSimpleName() + ", and area = " + withMaxArea.getArea());
+        log.info("Type of shape with min area  - " + withMinArea.getClass().getSimpleName() + ", and area = " + withMinArea.getArea());
     }
 
 
-}
+    }
+
+
+
